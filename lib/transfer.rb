7ca4d902fe30acb1c4ff4638c0638ea16e034ec0
Tony Baker <this.tonybaker@gmail.com>
2019-04-13 02:22:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -33,14 +33,14 @@ class Transfer
     end
   end
 
-  def execute_transaction
-    if self.sender.status = "closed"
-      "Transaction rejected. Please check your account balance."
-    elsif
-      self.status == "pending" && self.valid?
-      self.sender.balance -= self.amount
-      self.receiver.balance += self.amount
-      self.status = "complete"
-    end
-  end
+  # def execute_transaction
+  #   if self.sender.status = "closed"
+  #     "Transaction rejected. Please check your account balance."
+  #   elsif
+  #     self.status == "pending" && self.valid?
+  #     self.sender.balance -= self.amount
+  #     self.receiver.balance += self.amount
+  #     self.status = "complete"
+  #   end
+  # end
 end
